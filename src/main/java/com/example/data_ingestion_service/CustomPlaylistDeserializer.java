cff2b73a91a4c54a9d8cd63465fb0ad5f97c6a9a
Raunak-S <raunak.shrestha@gmail.com>
2025-04-30 18:10:39 -0400

    Add duration deserialization and deserializer tests

--- src/main/java/com/example/data_ingestion_service/CustomPlaylistDeserializer.java
+++ src/main/java/com/example/data_ingestion_service/CustomPlaylistDeserializer.java
@@ -1,18 +1,11 @@
 package com.example.data_ingestion_service;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
 public class CustomPlaylistDeserializer extends StdDeserializer<Playlist> {
     public CustomPlaylistDeserializer() {
@@ -24,21 +17,20 @@ public class CustomPlaylistDeserializer extends StdDeserializer<Playlist> {
     }
 
     @Override
-    public Playlist deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+    public Playlist deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
 
         String description = node.get("description").asText();
         int numFollowers = node.get("num_followers").intValue();
         String name = node.get("original_name").asText();
         String url = node.get("url").asText();
-        int tracks = node.get("tracks").size();
-//        List<Track> tracks = new ArrayList<>();
-//        for (JsonNode child : node.get("tracks")) {
-//            Track track = new Track();
-//            track.setDuration(child.get("duration_ms").intValue());
-//            track.setUrl(child.get("url").asText());
-//            tracks.add(track);
-//        }
-        return new Playlist(description, numFollowers, name, url, tracks);
+        int numTracks = 0;
+        int duration = 0;
+        for (JsonNode child : node.get("tracks")) {
+            numTracks += 1;
+            duration += child.get("duration_ms").intValue();
+        }
+
+        return new Playlist(description, numFollowers, name, url, numTracks, duration);
     }
 }
---
+++ src/test/java/com/example/data_ingestion_service/CustomPlaylistDeserializerTest.java
@@ -0,0 +1,35 @@
+package com.example.data_ingestion_service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CustomPlaylistDeserializerTest {
+
+    @Test
+    public void testDeserializeValidInput() throws IOException {
+        String json = "{\"description\":\"Sample playlist\",\"num_followers\":100,\"original_name\":\"Sample\",\"url\":\"https://www.sampleplaylist.com/\",\"tracks\":[{\"duration_ms\":120000,\"url\":\"https://www.sampletrack1.com/\"},{\"duration_ms\":240000,\"url\":\"https://www.sampletrack2.com/\"}]}";
+        Playlist p = new ObjectMapper().readValue(json, Playlist.class);
+
+        assertEquals("Sample", p.getName());
+        assertEquals("Sample playlist", p.getDescription());
+        assertEquals(100, p.getNumFollowers());
+        assertEquals(2, p.getNumTracks());
+        assertEquals("https://www.sampleplaylist.com/", p.getUrl());
+        assertEquals(360000, p.getDuration());
+    }
+
+    @Test
+    public void testDeserializeMalformedInput() throws IOException {
+        String json = "{\"description\":\"Sample playlist\",,,,,\"num_followers\":100,\"original_name\":\"Sample\",\"url\":\"https://www.sampleplaylist.com/\",\"tracks\":[{\"duration_ms\":120000,\"url\":\"https://www.sampletrack1.com/\"},{\"duration_ms\":240000,\"url\":\"https://www.sampletrack2.com/\"}]}";
+
+        assertThrows(IOException.class, () -> {
+            new ObjectMapper().readValue(json, Playlist.class);
+        });
+    }
+
+}
